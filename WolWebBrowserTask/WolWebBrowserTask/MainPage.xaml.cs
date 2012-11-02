bfbd9d656e3fc52f4dbae8effa30548bdc1b4707
Leonard Lee <sheeeng@gmail.com>
2012-11-02 14:08:20 +0100

    Implemented simple WebBrowserTask.

--- WolWebBrowserTask/WolWebBrowserTask/MainPage.xaml.cs
+++ WolWebBrowserTask/WolWebBrowserTask/MainPage.xaml.cs
@@ -10,15 +10,25 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Tasks;
 
 namespace WolWebBrowserTask
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        const string siteUrl = "http://m.wol.jw.org";
+
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            WebBrowserTask webBrowserTask = new WebBrowserTask();
+            webBrowserTask.Uri = new Uri(siteUrl, UriKind.Absolute);
+            webBrowserTask.Show();
         }
     }
 }
